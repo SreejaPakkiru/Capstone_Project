07a619e760d246ace2901764f999096474be5ab4
SreejaPakkiru <Sreeja.Pakkiru@WinWire.com>
2025-11-25 17:55:48 +0530

    chnaging smoketest

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
-#checking if services are running
-
-echo "Running Smoke Tests..."
-
-#Testing web1
-if curl -s http://localhost:5000/health > /dev/null; then
-    echo "web1 is up"
+if curl -s http://localhost:81 > /dev/null; then
+  echo "web1 is up"
 else
-    echo "web1 is down"
-    exit 1
+  echo "web1 is down"
+  exit 1
 fi
 
-#Testing web2
-if curl -s http://localhost:5001/health > /dev/null; then
-    echo "web2 is up"
+if curl -s http://localhost:82 > /dev/null; then
+  echo "web2 is up"
 else
-    echo "web2 is down"
-    exit 1
+  echo "web2 is down"
+  exit 1
 fi
-
-#Testing Redis
-if docker exec redis redis-cli ping | grep -q PONG; then
-    echo "Redis is up"
-else
-    echo "Redis is down"
-    exit 1
-fi
-
-echo "Passes Smoke Testing!"
